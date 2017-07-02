372974ccfcdf05f5bf255bf6e534394b4bc6464b
alpcanaydin <alpcan@alpcanaydin.com>
2017-07-02 19:36:50 +0300

    Typo fixed.

--- src/routes/Search.js
+++ src/routes/Search.js
@@ -24,7 +24,7 @@ const Search = () => (
         {' '}
         <Link to={`/baslik?q=recep tayyip erdoğan`}>recep tayyip erdoğan</Link>,
         {' '}
-        <Link to={`/yazar?q=ssg`}>@ssg</Link>
+        <Link to={`/yazar?q=ssg`}>@ssg</Link>,
         {' '}
         <Link to={`/yazar?q=kanzuk`}>@kanzuk</Link>
       </p>
